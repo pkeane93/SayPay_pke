585cc98d7d16508c12785cfcc617985ab26c170b
Pierre Keane <Pierrekeane@gmail.com>
2025-11-25 12:20:18 +0800

    Set up the pages and routes.

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,2 +1,6 @@
 class ChatsController < ApplicationController
+
+  def show
+  end
+
 end
--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,10 @@
 class TripsController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+  end
+
 end
