37f1c162cba32a7a4cbd8900f04bdaea78f38915
Pierre Keane <Pierrekeane@gmail.com>
2025-11-26 15:55:43 +0800

    created the create method of messagesController.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,21 @@
 class MessagesController < ApplicationController
+
+  def create
+    @chat = Chat.find(params[:chat_id])
+
+    @message = Message.new(message_params)
+    @message.chat = @chat
+    @message.role = "user"
+
+    if @message.save
+      llm_response = RubyLLM.chat.ask("Reply to the following message: #{@message.content}")
+      @llm_reply = Message.create!(chat: @chat, content: llm_response.content, role: "assistant")
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
 end
