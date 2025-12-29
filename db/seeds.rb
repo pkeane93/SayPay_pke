877015301c2e7fcfde8f297e05f3ea5112a8d7b4
Pierre Keane <Pierrekeane@gmail.com>
2025-12-29 18:25:42 +0700

    removed the commented out seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ Chat.destroy_all
 Trip.destroy_all
 User.destroy_all
 
-user = User.create!(email: "test@test.de", password: "123123", first_name: "Andreas", base_currency: "EUR")
+user = User.create!(email: "test@test.de", password: "123123", first_name: "Pierre", base_currency: "EUR")
 
 Trip.create!(
   country: "Laos",
@@ -33,19 +33,6 @@ first_trip = Trip.create!(
   start_date: Date.new(2025, 11, 6),
   end_date: Date.new(2025, 11, 14)
 )
-
-# last_trip = Trip.create!(
-#   country: "Indonesia",
-#   budget: 300.0,
-#   user_id: user.id,
-#   url: "https://images.unsplash.com/photo-1505993597083-3bd19fb75e57?ixid=M3w4Mzc4NzN8MHwxfHNlYXJjaHwxfHxpbmRvbmVzaWF8ZW58MHx8fHwxNzY0NzMwMDk3fDA&ixlib=rb-4.1.0&utm_source=SayPay&utm_medium=referral&utm_campaign=api-credit",
-#   start_date: Date.new(2025, 12, 14),
-#   end_date: Date.new(2025, 12, 28)
-# )
-
-# Expense.create!(category: "Meals", local_amount_cents: 200000, local_amount_currency: "IDR", base_amount_cents: 1000, base_amount_currency: user.base_currency, audio_transcript: "burger for 200k rupiah", notes: "none", trip_id: last_trip.id)
-# Expense.create!(category: "Shopping & Supplies", local_amount_cents: 300000, local_amount_currency: "IDR", base_amount_cents: 1500, base_amount_currency: user.base_currency, audio_transcript: "shirt for 300k rupiah", notes: "none", trip_id: last_trip.id)
-# Expense.create!(category: "Meals", local_amount_cents: 56250, local_amount_currency: "IDR", base_amount_cents: 500, base_amount_currency: user.base_currency, audio_transcript: "coffee for 100k rupiah thai bath", notes: "none", trip_id: last_trip.id)
 
 Expense.create!(category: "Shopping & Supplies", local_amount_cents: 1870100, local_amount_currency: "THB", base_amount_cents: 50000, base_amount_currency: user.base_currency, audio_transcript: "designer t-shirt", notes: "none", trip_id: first_trip.id)
 Expense.create!(category: "Meals", local_amount_cents: 14961, local_amount_currency: "THB", base_amount_cents: 400, base_amount_currency: user.base_currency, audio_transcript: "dinner", notes: "none", trip_id: first_trip.id)
